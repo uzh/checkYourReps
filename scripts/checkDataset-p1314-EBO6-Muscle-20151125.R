dc4089a493a47a422d8828fce76974235654c3bd
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2016-01-05 17:20:05 +0100

    update example

--- scripts/checkDataset-p1314-EBO6-Muscle-20151125.R
+++ scripts/checkDataset-p1314-EBO6-Muscle-20151125.R
@@ -1,6 +1,8 @@
 
 require(ezRun)
-WORK_DIR = "~/R/checkYourReps-data"
+WORK_DIR <<- "~/R/checkYourReps-data"
+ENRICHR_GENELIST_DIRECTORY <<- "/srv/GT/databases/enrichr"
+MIN_GENESET_SIZE <<- 5
 
 setwd(WORK_DIR)
 
@@ -15,6 +17,7 @@ param = list()
 param[['refBuild']] = countDs$getColumn("refBuild")[1]
 param$dataRoot = "/srv/gstore/projects"
 param$sigThresh = 50
+param$bgExpression = 25
 param[['refFeatureFile']] = countDs$getColumn("refFeatureFile")[1]
 param[['featureLevel']] = 'gene'
 param[['normMethod']] = 'logMean'
@@ -25,43 +28,77 @@ param = ezParam(param)
 # save(countRd, file="countRd.RData")
 load("countRd.RData")
 countRd$signal = ezNorm(countRd$counts, method=param$normMethod, presentFlag = countRd$presentFlag)
-seqAnno = countRd$seqAnno
-geneCats = inverseMapping(goStringsToList(seqAnno$`GO BP`, seqAnno$gene_name))
-geneCats = geneCats[sapply(geneCats, length) >= 5]
-geneCatIdxs = lapply(geneCats, function(gn){match(gn, seqAnno$gene_name)})
+geneSymbols = countRd$seqAnno$hgnc_symbol
 
 
+# ########## get gene lists
+# geneCats = inverseMapping(goStringsToList(seqAnno$`GO BP`, seqAnno$gene_name))
+# geneCats = geneCats[sapply(geneCats, length) >= 5]
+# geneCatIdxs = lapply(geneCats, function(gn){match(gn, geneSymbols)})
+
+geneListFiles = list.files(ENRICHR_GENELIST_DIRECTORY, pattern=".gmt$", full.names = TRUE)
+names(geneListFiles) = sub(".gmt$", "", basename(geneListFiles))
+
+
+## prepare the data ------------
 replicateGroups = split(countDs$getNames(), countDs$getColumn(replicateGroupingVariable))
-
-require(edgeR)
-
 repSamples = replicateGroups[[1]] ## repeat for each set of replicates
 countDataUse = selectSamples(countRd, repSamples)
-
 selectedSample = repSamples[1] ## repeat for each sample
+controlSamples = setdiff(repSamples, selectedSample)
+
+## get gene set information ready ----
+# repeat for each gene list file
+gmtFile = geneListFiles[1]
+geneSets = readGmt(gmtFile)
+geneSets = geneSets[sapply(geneSets, length) >= MIN_GENESET_SIZE]
+geneSetIndices = lapply(geneSets, function(gn){na.omit(match(gn, geneSymbols))})
 
 
 ###### analysis using mroast
+require(edgeR)
 repFactor = repSamples %in% selectedSample
 design = model.matrix(~ repFactor)
-
-mroastResult = mroast(countDataUse$counts, index=geneCatIdxs, design = design)
+mroastResult = mroast(countDataUse$counts, index=geneSetIndices, design = design)
 head(mroastResult[order(mroastResult$PValue.Mixed), ])
 
 
 #### analysis using camera
-cameraResult = camera(countDataUse$counts, index=geneCatIdxs, design = design)
+require(edgeR)
+cameraResult = camera(countDataUse$counts, index=geneSetIndices, design = design)
 head(cameraResult[order(cameraResult$PValue), ])
 
+## analysis using clusterProfiler
+require(clusterProfiler)
+## we need to generate a ranked gene list. We rank according to the log2 ratio
+x = log2(countRd$signal + param$bgExpression)
+scores = x[ , selectedSample] - rowMeans(x[ , controlSamples])
+names(scores) = geneSymbols
+sortedScores = sort(scores)
+term2Gene = ezFrame(term=rep(names(geneSets), sapply(geneSets, length)), gene=unlist(geneSets))
 
-### analysis using GSEA
+enricherResult = enricher(names(sortedScores)[1:50], minGSSize = MIN_GENESET_SIZE, TERM2GENE=term2Gene, pvalueCutoff = 1 )
+head(summary(enricherResult))
+
+gseaResult = GSEA(geneList = sortedScores, minGSSize = 10, TERM2GENE=term2Gene[term2Gene$gene %in% names(sortedScores), ], 
+                  verbose = TRUE, pvalueCutoff = 1, nPerm = 1000  )
+## WARNING this function will use all cores on your system
+## you can change this only be overwriting DOSE::gsea
+## with nperm > 30 and minGSSize = 3 this tends to fail with the error: Error in sign(ES) : non-numeric argument to mathematical function
+## and warnings saying:  In mean.default(x[x >= 0]) : argument is not numeric or logical: returning NA
+## apparently a problem of two small gene set size which can cause problems in some permutations
+head(summary(gseaResult))
+
+
+## other approaches:
+## - gage
+## - segGSEA
+## - Category::gseattperm
+
+## different scores as input for GSEA!
+## normalization with different gene universe sets!
 
 
 
-
-
-
-
-#goana.default
 
 
